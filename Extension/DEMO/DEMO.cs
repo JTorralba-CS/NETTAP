7301df16c0c72651c4b7f6da1042fb65cb7a686a
Julius Torralba <Julius.Torralba@GMail.com>
2021-03-07 22:20:03 -0700

    Implement DEMO extension.

---
+++ Extension/DEMO/DEMO.cs
@@ -0,0 +1,29 @@
+﻿using Core;
+using System;
+using System.Net;
+
+namespace DEMO
+{
+    public class DEMO : Interface.Extension
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public Byte Priority { get; set; }
+        public Byte[] Mutant { get; set; }
+        public int Mutant_Size { get; set; }
+
+        public DEMO()
+        {
+            Name = "DEMO";
+            Description = "This is the DEMO extension.";
+            Priority = 20;
+        }
+
+        public int Execute(IPEndPoint Source, IPEndPoint Destination, Byte[] Packet, int Packet_Size)
+        {
+            Log.Terminal(Source.Address + ":" + Source.Port.ToString() + " ---> " + Destination.Address + ":" + Destination.Port.ToString(), Packet, Packet_Size);
+
+            return 0;
+        }
+    }
+}
